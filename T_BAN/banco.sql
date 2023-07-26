dd0a6086715f9af0517f91d9a4837ab08681be7a
debora <86936640+deboralawall@users.noreply.github.com>
2023-07-26 16:14:46 -0300

    banco.sql

--- T_BAN/banco.sql
+++ T_BAN/banco.sql
@@ -19,8 +19,8 @@ CREATE TABLE conta (
 );
 
 -- Uma opção para inserir novas tuplas da primeira tabela
-INSERT INTO cliente (cpf, nome, rua, bairro, numero, telefone) VALUES ('12789508061','debora','Paulo M','Bom Retiro',120,97735427);
-INSERT INTO cliente (cpf, nome, rua, bairro, numero, telefone) VALUES ('12260007960','monique','123','Bom Retiro',900,99405427);
+INSERT INTO cliente (cpf, nome, rua, bairro, numero, telefone) VALUES ('13389598091','debora','Paulo M','Bom Retiro',120,99775522);
+INSERT INTO cliente (cpf, nome, rua, bairro, numero, telefone) VALUES ('12260007960','monique','123','Bom Retiro',900,99445527);
 
 -- Uma opção para inserir novas tuplas da segunda tabela
 INSERT INTO conta (saldo, cpf) VALUES (1000,'12789507961');
